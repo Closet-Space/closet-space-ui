6f2fff554c34dac2f3b1db84569f46bd7157c7be
Raul Guajardo <hitomisparda@gmail.com>
2020-02-23 01:52:00 -0500

    Convert to scss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserSignUpFormComponent } from './user-sign-up-form/user-sign-up-form.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'signup', component: UserSignUpFormComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
