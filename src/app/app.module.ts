9de620e08ff41ade42e4efff0956927a98a3fdeb
Raul Guajardo <hitomisparda@gmail.com>
2020-02-23 01:13:27 -0500

    Added components for landing page and user sign up form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserSignUpFormComponent } from './user-sign-up-form/user-sign-up-form.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSignUpFormComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-sign-up-form/user-sign-up-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-sign-up-form',
+  templateUrl: './user-sign-up-form.component.html',
+  styleUrls: ['./user-sign-up-form.component.sass']
+})
+export class UserSignUpFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
