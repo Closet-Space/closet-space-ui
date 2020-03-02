d5cc6dec3c9b3aa6f6da49c7291cf02d069654a4
Raul Guajardo <hitomisparda@gmail.com>
2020-03-01 21:56:27 -0500

    Changed to scss, easier to work with

--- src/app/user-sign-up-form/user-sign-up-form.component.ts
+++ src/app/user-sign-up-form/user-sign-up-form.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-user-sign-up-form',
-  templateUrl: './user-sign-up-form.component.html',
-  styleUrls: ['./user-sign-up-form.component.sass']
+    selector: 'app-user-sign-up-form',
+    templateUrl: './user-sign-up-form.component.html',
+    styleUrls: ['./user-sign-up-form.component.scss']
 })
 export class UserSignUpFormComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
